6b895216e4a55653f6e805642a78a0aa2d0e2295
Geloger37 <69764552+Geloger37@users.noreply.github.com>
2022-02-07 12:23:18 +0800

    Вывод на экран с помощью системного вызова write

--- 1/1.c
+++ 1/1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-//#include "fact.h"
 
-fact(int n) {
+fact(n) {
 	return n == 0 ? 1 : n*fact(n-1);
 }
 
 main() {
 	int n = 10;
 	puts("Hello, world!");
-	printf("Fact of %d is %d", n, fact(n));
+	printf("Fact of %d is %d\n", n, fact(n));
+	char* hello = "I am a system call!";
+	write(1, hello, strlen(hello));
 	return 0;
 }
