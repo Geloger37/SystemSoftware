74dd368a6d5da215a5dbc063692bb0e74120c2c9
Geloger37 <69764552+Geloger37@users.noreply.github.com>
2022-02-08 10:08:01 +0800

    Форматированная строка с помощью write

--- 1/1.c
+++ 1/1.c
@@ -9,7 +9,10 @@ fact(n) {
 main() {
 	int n = 10;
 	puts("Hello, world!");
-	printf("Fact of %d is %d\n", n, fact(n));
+	//printf("Fact of %d is %d\n", n, fact(n));
+	char f[1024];
+	int length = snprintf(f, 1024, "Fact of %d is %d\n", n, fact(n));
+	write(1, f, length);
 	char* hello = "I am a system call!";
 	write(1, hello, strlen(hello));
 	return 0;
